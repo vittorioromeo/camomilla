130d95c47f1e050b7b4c43e851a44403181e2abf
Vittorio Romeo <vittorio.romeo@outlook.com>
2016-09-09 00:22:19 +0100

    Setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "camomilla",
+    version = "0.1",
+    packages = find_packages(),
+    author = "Vittorio Romeo",
+    author_email = "vittorio.romeo@outlook.com",
+    description = "C++ error postprocessor"
+)
