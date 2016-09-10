92142f1dc42226bf5eea2cfa289fb4f66b1c753e
Vittorio Romeo <vittorio.romeo@outlook.com>
2016-09-10 22:14:17 +0100

    Update setup, readme, add images

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import setup, find_packages
 
 setup(
     name = "camomilla",
+    scripts=['camomilla'],
     version = "0.1",
-    packages = find_packages(),
     author = "Vittorio Romeo",
     author_email = "vittorio.romeo@outlook.com",
     description = "C++ error postprocessor"
