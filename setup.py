14747fc959310d215fa0eb88e98cff29f46610dd
Vittorio Romeo <vittorio.romeo@outlook.com>
2016-09-18 16:22:07 +0100

    Up version to 0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(
     name = "camomilla",
     scripts=['camomilla'],
-    version = "0.1",
+    version = "0.2",
     author = "Vittorio Romeo",
     author_email = "vittorio.romeo@outlook.com",
     description = "C++ error postprocessor"
